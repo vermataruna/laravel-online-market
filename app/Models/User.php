bba3906427c2c8e0629f81145470b6673cdd5111
vermataruna <tarunaverma792@gmail.com>
2024-02-15 23:56:24 +0530

    add: models fillable attributes and methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,39 +7,50 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    CONST VERIFIED_USER = '1';
+    CONST UNVERIFIED_USER = '0';
+    
+    CONST ADMIN_USER = 'true';
+    CONST REGULAR_USER = 'false';
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'verified',
+        'verification_token',
+        'admin',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
+        'verification_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function isAdmin(): bool
+    {
+        return $this->admin === User::ADMIN_USER;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->verified === User::VERIFIED_USER;
+    }
+
+    public static function generateVerificationCode(): string
+    {
+        return Str::random(40);
+    }
 }
