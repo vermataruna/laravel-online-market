53e04527f25f549be3b94d03adbeaa668a85e28f
vermataruna <tarunaverma792@gmail.com>
2024-02-15 23:27:50 +0530

    add: basic routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Transaction\TransactionController;
+use App\Http\Controllers\User\UserController;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +24,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Buyers
+ Route::resource('buyers', BuyerController::class)->only(['index', 'show']);
+
+ //Categories
+ Route::resource('categories', CategoryController::class)->except(['create', 'edit']);
+
+ //Products
+ Route::resource('products', ProductController::class)->only(['index', 'show']);
+
+ //Sellers
+ Route::resource('sellers', SellerController::class)->only(['index', 'show']);
+
+ //Transactions
+ Route::resource('transactions', TransactionController::class)->only(['index', 'show']);
+
+ //Users
+ Route::resource('users', UserController::class)->except(['create', 'edit']);
